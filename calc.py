51ef201b59d40744de551d7d02d68d1f68e8e42e
David <pentru.diverse.02@gmail.com>
2026-06-23 12:56:58 +0300

    calc simplu

---
+++ calc.py
@@ -0,0 +1,47 @@
+
+class Calculator:
+    def __init__(self):
+        self.a=0
+        self.b=0
+    def add(self):
+        return self.a+self.b
+    def sub(self):
+        return self.a-self.b
+    def mul(self):
+        return self.a*self.b
+    def div(self):
+        return self.a/self.b
+    def setA(self):
+        self.a=float(input("a:"))
+    def setB(self):
+        self.b=float(input("b:"))
+
+    def pornesteCalculator(self):
+
+
+        print(f"  a = {self.a}   b = {self.b}")
+        print("1. Add")
+        print("2. Subtract")
+        print("3. Multiply")
+        print("4. Divide")
+        print("5. setA")
+        print("6. setB")
+        a=int(input("Enter your choice:"))
+        match a:
+            case 1:
+                print(self.add())
+            case 2:
+                print(self.sub())
+            case 3:
+                print(self.mul())
+            case 4:
+                print(self.div())
+            case 5:
+                self.setA()
+            case 6:
+                self.setB()
+        self.pornesteCalculator()
+
+if __name__ == '__main__':
+    calculator=Calculator()
+    calculator.pornesteCalculator()
